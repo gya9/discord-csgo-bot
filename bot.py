ecb4462c04029f8059f655a88470a9c1d6fdbc93
Ubuntu <ubuntu@ip-172-31-39-220.us-east-2.compute.internal>
2018-08-10 16:12:07 +0000

    second

---
+++ bot.py
@@ -0,0 +1,48 @@
+import discord
+from matching import point_mirage, point_cache, match_mirage, match_cache
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('------')
+
+allhito = {"arch", "darknist", "3ple", "gya9", "keshigomu", "toka", "yukiya", "bekutoru"}
+
+@client.event
+async def on_message(message):
+    # 「おはよう」で始まるか調べる
+    if message.content.startswith("おはよう"):
+        # 送り主がBotだった場合反応したくないので
+        if client.user != message.author:
+            # メッセージを書きます
+            m = "おはようございます" + message.author.name + "さん！"
+            # メッセージが送られてきたチャンネルへメッセージを送ります
+            await client.send_message(message.channel, m)
+
+    if message.content.startswith("!help"):
+        if client.user != message.author:
+            m1 = "メンバー一覧:"+str(allhito)
+            m2 = "!match (メンバー)とかいてね！　空白はmatchの後にひとつだけ、メンバーは,で区切り"
+            await client.send_message(message.channel, m1)
+            await client.send_message(message.channel, m2)
+
+    if message.content.startswith("!match"):
+        if client.user != message.author:
+            hito = message.content[7:]
+            hitoset = set(hito.split(","))
+
+            # if not hitoset <= allhito:
+                # error = "エラー：メンバーがおかしいです　入力にミスがあるかも"
+
+            cache = match_cache(hitoset)
+            mirage = match_mirage(hitoset)
+
+            # await client.send_message(message.channel, error)
+            await client.send_message(message.channel, cache)
+            await client.send_message(message.channel, mirage)
+
+
+client.run("NDc3MTUyMTI5MDIzMzQ0NjQw.Dk3-1A.Q0Gw2pQgXCEuiOxuEDKBNUZVszk")
---
+++ matching.py
@@ -0,0 +1,147 @@
+allhito = {"arch", "darknist", "3ple", "gya9", "keshigomu", "toka", "yukiya", "bekutoru"}
+
+point_cache = {
+    "arch":[1,3,1],\
+    "darknist":[5,0,0],\
+    "3ple":[0,0,5],\
+    "gya9":[1,1,1],\
+    "keshigomu":[4,1,0],\
+    "toka":[1,1,3],\
+    "yukiya":[2,0,3],\
+    "bekutoru":[1,1,1],\
+    }
+
+point_mirage = {
+    "arch":[1,3,1],\
+    "darknist":[0,0,5],\
+    "3ple":[4,0,1],\
+    "gya9":[1,4,0],\
+    "keshigomu":[0,0,5],\
+    "toka":[3,0,2],\
+    "yukiya":[0,2,3],\
+    "bekutoru":[3,2,0],\
+    }
+
+def match_mirage(S):
+    """mirageのマッチング関数。引数はset型"""
+    SS = list(S)
+    result = [[],[],[]]
+
+    # print(SS)
+
+    #mid決め
+    highscore = -1
+    for hito in SS:
+        tmp = point_mirage[hito][1]
+        if tmp > highscore:
+            highscore = tmp
+            ans = hito
+    # print("added",ans,"to mid")
+    result[1].append(SS.pop(SS.index(ans)))
+
+    # print(SS)
+
+    #A決め(1位)
+    highscore = -1
+    for hito in SS:
+        tmp = point_mirage[hito][0]
+        if tmp > highscore:
+            highscore = tmp
+            ans = hito
+    # print("added",ans,"to A")
+    result[0].append(SS.pop(SS.index(ans)))
+
+    # print(SS)
+
+    #B決め(1位)
+    highscore = -1
+    for hito in SS:
+        tmp = point_mirage[hito][2]
+        if tmp > highscore:
+            highscore = tmp
+            ans = hito
+    # print("added",ans,"to B")
+    result[2].append(SS.pop(SS.index(ans)))
+
+    # print(SS)
+
+    #A決め(2位)
+    highscore = -1
+    for hito in SS:
+        tmp = point_mirage[hito][0]
+        if tmp > highscore:
+            highscore = tmp
+            ans = hito
+    # print("added",ans,"to A")
+    result[0].append(SS.pop(SS.index(ans)))
+
+    #B決め(2位)
+    result[2].append(SS.pop(0))
+
+    a = result[0]
+    mid = result[1]
+    b = result[2]
+
+    return("【mirage】A:" + str(a) + "  MID:" + str(mid) + "  B:" + str(b))
+
+def match_cache(S):
+    """cacheのマッチング関数。引数はset型"""
+    SS = list(S)
+    result = [[],[],[]]
+
+    # print(SS)
+
+    #mid決め
+    highscore = -1
+    for hito in SS:
+        tmp = point_cache[hito][1]
+        if tmp > highscore:
+            highscore = tmp
+            ans = hito
+    # print("added",ans,"to mid")
+    result[1].append(SS.pop(SS.index(ans)))
+
+    # print(SS)
+
+    #A決め(1位)
+    highscore = -1
+    for hito in SS:
+        tmp = point_cache[hito][0]
+        if tmp > highscore:
+            highscore = tmp
+            ans = hito
+    # print("added",ans,"to A")
+    result[0].append(SS.pop(SS.index(ans)))
+
+    # print(SS)
+
+    #B決め(1位)
+    highscore = -1
+    for hito in SS:
+        tmp = point_cache[hito][2]
+        if tmp > highscore:
+            highscore = tmp
+            ans = hito
+    # print("added",ans,"to B")
+    result[2].append(SS.pop(SS.index(ans)))
+
+    # print(SS)
+
+    #A決め(2位)
+    highscore = -1
+    for hito in SS:
+        tmp = point_cache[hito][0]
+        if tmp > highscore:
+            highscore = tmp
+            ans = hito
+    # print("added",ans,"to A")
+    result[0].append(SS.pop(SS.index(ans)))
+
+    #B決め(2位)
+    result[2].append(SS.pop(0))
+
+    a = result[0]
+    mid = result[1]
+    b = result[2]
+
+    return("【cache】A:" + str(a) + "  MID:" + str(mid) + "  B:" + str(b))
